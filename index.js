07f49f9a1daef09389332d6ac4ef6716887140f5
Kyle Calica <kyle@getpostman.com>
2019-10-18 13:26:07 -0700

    fixed readFile

--- index.js
+++ index.js
@@ -10,29 +10,21 @@ try {
   const AWS_SECRET_ID = core.getInput('AWS_SECRET_ID');
   const AWS_REGION = core.getInput('AWS_REGION');
   console.log(`Updating Bucket ${bucketName} with ${package}!`);
-  var body = null;
   const s3 = new AWS.S3({apiVersion: '2006-03-01', accessKeyId: AWS_SECRET_ID, secretAccessKey:AWS_SECRET_KEY, region:AWS_REGION  });
-  fs.readFileSync(`./${package}`, (err,data) => {
-    if(err){
-      console.log(err);
-      core.setFailed(err);
-    } else {
-      console.log(data);
-      const params = {
-       Body: data,
-       Bucket: bucketName
-       };
-       s3.upload(params, (err, data) => {
-         if(err){
-           console.log(`Failed upload to ${bucketName}`);
-           throw err;
-         } else {
-           console.log(`Succesful upload to ${bucketName}`);
-         }
-       });
-    }
-  });
-
+  var body = fs.readFileSync(`./${package}`);
+  console.log(body);
+  const params = {
+   Body: body,
+   Bucket: bucketName
+   };
+   s3.upload(params, (err, data) => {
+     if(err){
+       console.log(`Failed upload to ${bucketName}`);
+       throw err;
+     } else {
+       console.log(`Succesful upload to ${bucketName}`);
+     }
+   });
 
 } catch (error) {
  core.setFailed(error.message);
