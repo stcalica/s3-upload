9aa07699b54f41958707c36a6810ffbf8cf79438
Kyle Calica <kyle@getpostman.com>
2019-10-18 10:55:43 -0700

    fixed s3 registration

--- index.js
+++ index.js
@@ -11,15 +11,20 @@ try {
   const AWS_REGION = core.getInput('AWS_REGION');
   console.log(`Updating Bucket ${bucketName} with ${package}!`);
 
-  var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+  const s3 = new AWS.S3({apiVersion: '2006-03-01', accessKeyId: AWS_SECRET_ID, secretAccessKey:AWS_SECRET_KEY, region:AWS_REGION  });
   var body = fs.open(`./${package}`);
 
-  var params = {
+  const params = {
    Body: body,
    Bucket: bucketName
    };
-   s3.upload(params, function(err, data) {
-     console.log(err, data);
+   s3.upload(params, function uploadCallback(err, data) {
+     if(err){
+       console.log(`Failed upload to ${bucketName}`);
+       throw err;
+     } else {
+       console.log(`Succesful upload to ${bucketName}`);
+     }
    });
 
 } catch (error) {
